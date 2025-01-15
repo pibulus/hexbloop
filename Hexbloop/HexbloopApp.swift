508f3ad52fdb0a714661ec9d148b4880873c5989
Pablo Alvarado <pibulus@gmail.com>
2025-01-15 19:09:29 +1100

    Initial Commit

---
+++ Hexbloop/HexbloopApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexbloopApp.swift
+//  Hexbloop
+//
+//  Created by Pablo Alvarado on 15/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct HexbloopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HexbloopTests/HexbloopTests.swift
@@ -0,0 +1,17 @@
+//
+//  HexbloopTests.swift
+//  HexbloopTests
+//
+//  Created by Pablo Alvarado on 15/1/2025.
+//
+
+import Testing
+@testable import Hexbloop
+
+struct HexbloopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
